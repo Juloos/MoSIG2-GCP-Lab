f4fa31da2b8e010dc7d991e136fe3ebb4fd0a3b9
Juloos <juloos.sfr@gmail.com>
2024-12-08 16:52:18 +0100

    ✨ delayed auto delete cluster when using start script

--- create_cluster.sh
+++ create_cluster.sh
@@ -5,3 +5,5 @@ cd $(dirname $0)
 source .env
 
 gcloud container clusters create online-boutique -m ${MACHINE_TYPE} --project=${PROJECT_ID} --region=${REGION} --num-nodes=${NUM_NODES}
+
+( sleep 12h ; ./delete_cluster.sh ) &
