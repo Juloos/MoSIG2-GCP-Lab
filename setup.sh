d18facb4e5b89c435a601d762af4799dfb24b243
Juloos <juloos.sfr@gmail.com>
2025-01-09 16:39:13 +0100

    don't always try to install terraform

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ cd $(dirname $0)
 git clone --depth 1 --branch v0 https://github.com/GoogleCloudPlatform/microservices-demo.git
 cd microservices-demo
 
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+if [ -z "$(command -v terraform)" ]; then
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install terraform
+fi
 
 terraform init
